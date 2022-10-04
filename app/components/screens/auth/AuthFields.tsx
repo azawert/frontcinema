eaf577e8622f60cb90f012b5152a60cb363df8ee
Igor <azawertik@gmail.com>
2022-10-04 23:41:55 +0300

    added password validation

--- app/components/screens/auth/AuthFields.tsx
+++ app/components/screens/auth/AuthFields.tsx
@@ -1,21 +1,45 @@
-import React, {FC} from 'react';
-import {FormState, UseFormRegister} from "react-hook-form";
-import Input from "@/ui/form-elements/Input";
-import {validEmail} from "@/shared/regex";
+import React, { FC } from 'react'
+import { FormState, UseFormRegister } from 'react-hook-form'
+
+import Input from '@/ui/form-elements/Input'
+
+import { validEmail } from '@/shared/regex'
+
 interface IAuthFields {
-    register: UseFormRegister<any>,
-    formState: FormState<any>,
-    isPasswordRequired? : boolean
+	register: UseFormRegister<any>
+	formState: FormState<any>
+	isPasswordRequired?: boolean
 }
 
-const AuthFields:FC<IAuthFields> = ({isPasswordRequired = false,register,formState:{errors}}) => {
+const AuthFields: FC<IAuthFields> = ({
+	isPasswordRequired = false,
+	register,
+	formState: { errors },
+}) => {
+	return (
+		<>
+			<Input
+				{...register('email', {
+					required: 'Email is required',
+					pattern: { value: validEmail, message: 'Please enter valid email' },
+				})}
+				placeholder={'Email'}
+				error={errors.email}
+			/>
+			<Input
+				{...register('password', {
+					required: 'password is required',
+					minLength: {
+						value: 5,
+						message: 'Min length should more than 5 symbols',
+					},
+				})}
+				type={'password'}
+				placeholder={'Password'}
+				error={errors.password}
+			/>
+		</>
+	)
+}
 
-    return (
-        <>
-            <Input {...register('email',{required:'Email is required',pattern:{value:validEmail,message:'Please enter valid email'}})} placeholder={'Email'} error={errors.email} />
-            <Input {...register('password',{required:'password is required',minLength: {value:5,message:'Min length should more than 5 symbols'}})} type={"password"} placeholder={'Password'}  />
-        </>
-    );
-};
-
-export default AuthFields;
+export default AuthFields
